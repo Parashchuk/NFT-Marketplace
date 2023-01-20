f85de627e8245d988e68f2f076fb35fe6fdd63b7
Andrii Parashchuk <anypaster@Andriis-MacBook-Pro.local>
2023-01-20 15:25:30 +0100

    fix issue with categories image, and implement profile page

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Landing from './pages/landing';
 import './assets/style/index.scss';
 import NotFound from './pages/notFound';
 import Login from './pages/login';
+import Profile from './pages/profile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Landing />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/profile' element={<Profile />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,19 @@
 const Profile = () => {
   return (
     <>
-      <div className='profile'></div>
+      <div className='profile'>
+        <div className='profile__container'>
+          <div className='profile__container__header'>
+            <div className='profile__container__header__placeholder'>
+              <img src='' alt='' />
+            </div>
+            <div className='profile__container__header__avatar'></div>
+          </div>
+          <div className='profile__container__user-info'></div>
+          <div className='porfile__container__filters'></div>
+          <div className='profile__container__nft-cards'></div>
+        </div>
+      </div>
     </>
   );
 };
