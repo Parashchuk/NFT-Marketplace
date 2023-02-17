c5bd66968305f890d44947d4cd3812a89d46bcab
Parashchuk <Andry.parashchuk@gmail.com>
2023-02-17 08:50:39 +0100

    Discover section implement

--- src/components/landing/discover.jsx
+++ src/components/landing/discover.jsx
@@ -5,14 +5,10 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Discover = () => {
-  const { isLoading } = useSelector((state) => state.collections);
-  const discoverCollection = useSelector((state) => state.collections.data[0]);
-
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    console.log(isLoading, discoverCollection);
-  });
+  const { isLoading } = useSelector((state) => state.collections);
+  const discoverCollection = useSelector((state) => state.collections.data[0]);
 
   useEffect(() => {
     dispatch(fetchCollections(1));
@@ -20,8 +16,8 @@ const Discover = () => {
 
   if (isLoading) return <Preloader />;
   return (
-    <section className='discover'>
-      <div className='discover__container'>
+    <section className='discover wrapper'>
+      <div className='discover__container wrapper-container'>
         <div className='discover__container__description'>
           <div className='discover__container__description__wrap'>
             <div className='discover__container__description__title'>Discover More NFTs</div>
@@ -29,7 +25,9 @@ const Discover = () => {
               Explore new trending NFTs
             </div>
           </div>
-          <div className='discover__container__description__subtitle__button'>See All</div>
+          <div className='discover__container__description__subtitle__button button-template button-secondary'>
+            See All
+          </div>
         </div>
         <div className='discover__container__nfts'>
           {discoverCollection &&
@@ -45,18 +43,20 @@ const Discover = () => {
                     </div>
                     <div className='discover__container__nfts__cart__description__author'>
                       <div className='discover__container__nfts__cart__description__author__img'>
-                        {discoverCollection.author.avatar}
+                        <img src={discoverCollection.author.avatar} alt='avatar' />
                       </div>
                       <div className='discover__container__nfts__cart__description__author__name'>
                         {discoverCollection.author.username}
                       </div>
                     </div>
                     <div className='discover__container__nfts__cart__description__worth'>
-                      <div className='discover__container__nfts__cart__description__worth__price'>
-                        {nft.price}
+                      <div className='discover__container__nfts__cart__description__worth__value'>
+                        <span>Price</span>
+                        {nft.price ? nft.price : 0} ETH
                       </div>
-                      <div className='discover__container__nfts__cart__description__worth__bid'>
-                        {nft.bidHistory}
+                      <div className='discover__container__nfts__cart__description__worth__value'>
+                        <span>Highest Bid</span>
+                        {nft.bidHistory.length ? nft.bidHistory[0] : 0} ETH
                       </div>
                     </div>
                   </div>
