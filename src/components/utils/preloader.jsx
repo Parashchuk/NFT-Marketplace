b7373e85dff94dd0176dec6a6a3b7e281e12c898
Parashchuk <Andry.parashchuk@gmail.com>
2023-02-24 23:41:28 +0100

    update preloader

--- src/components/utils/preloader.jsx
+++ src/components/utils/preloader.jsx
@@ -1,9 +1,22 @@
-import preloader from '../../assets/img/svg/preloader.svg';
+import { useState } from 'react';
+import preloader from '../../assets/img/svg/preloader.gif';
 
 const Preloader = () => {
+  const [isServerLoading, setIsServerLoading] = useState(false);
+
+  setTimeout(() => {
+    setIsServerLoading(true);
+  }, 2500);
+
   return (
     <div className='preloader'>
       <img src={preloader} alt='preloader' />
+      {isServerLoading && (
+        <div>
+          <span>Please wait till we start server</span>
+          <span>It's always about 15 seconds</span>
+        </div>
+      )}
     </div>
   );
 };
