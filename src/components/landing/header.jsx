ead3e005875db2aed823dd8fd81f50747f5dc378
Parashchuk <Andry.parashchuk@gmail.com>
2023-02-25 09:00:19 +0100

    Implement burger menu

--- src/components/landing/header.jsx
+++ src/components/landing/header.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import BurgetButton from '../../assets/img/svg/list.svg';
 import LogoText from '../../assets/img/svg/logo.svg';
 import LogoImage from '../../assets/img/svg/storefront.svg';
 import User from '../../assets/img/svg/user.svg';
+import Close from '../../assets/img/svg/close.svg';
 
 const LandingHeader = () => {
+  const [burgerMenuStatus, setBurgerMenuStatus] = useState(false);
+
   return (
     <header>
       <div className='landingHeader'>
@@ -14,11 +18,18 @@ const LandingHeader = () => {
           <img className='landingHeader__logo-text' src={LogoText} alt='LogoText' />
         </Link>
         <nav>
-          <img className='landingHeader__burger-menu' src={BurgetButton} alt='List' />
+          <img
+            onClick={() => setBurgerMenuStatus(!burgerMenuStatus)}
+            className={
+              burgerMenuStatus ? 'landingHeader__burger-menu-close' : 'landingHeader__burger-menu'
+            }
+            src={burgerMenuStatus ? Close : BurgetButton}
+            alt='List'
+          />
           <ul className='landingHeader__navigation'>
             <li>
               <Link className='navigation-item' to='#'>
-                Marketplace
+                Collections
               </Link>
             </li>
             <li>
@@ -41,6 +52,33 @@ const LandingHeader = () => {
               </Link>
             </li>
           </ul>
+          {burgerMenuStatus && (
+            <ul className='landingHeader__burger-navigation'>
+              <li>
+                <Link className='burger-navigation-item' to='#'>
+                  Marketplace
+                </Link>
+              </li>
+              <li>
+                <Link className='burger-navigation-item' to='#'>
+                  Rankings
+                </Link>
+              </li>
+              <li>
+                <Link className='burger-navigation-item' to='#'>
+                  Connect a wallet
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className='burger-navigation-item burger-navagation-item--button button-template button-secondary'
+                  to='/register'>
+                  <img src={User} alt='user' />
+                  <span>Sign Up</span>
+                </Link>
+              </li>
+            </ul>
+          )}
         </nav>
       </div>
     </header>
