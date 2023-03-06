6f4dc5cc5c7753487d999f75362dd6f7be3e82a1
Parashchuk <Andry.parashchuk@gmail.com>
2023-03-06 16:30:40 +0100

    some optimisation

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,30 @@
-import { Routes, Route } from 'react-router-dom';
-
 import Register from './pages/landing/register';
 import Landing from './pages/landing/landing';
 import NotFound from './pages/landing/notFound';
 import Login from './pages/landing/login';
 import ConnectWallet from './pages/landing/connectWallet';
 import GetStarted from './pages/landing/getStarted';
+import Preloader from './components/utils/preloader';
 import './assets/style/index.scss';
 
+import { fetchCollections } from './store/reducers/collections';
+import { fetchUsers } from './store/reducers/users';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 function App() {
+  const dispatch = useDispatch();
+  const isCollectionsLoading = useSelector((state) => state.collections.isLoading);
+  const isUsersLoading = useSelector((state) => state.users.isLoading);
+
+  useEffect(() => {
+    dispatch(fetchCollections(5));
+    dispatch(fetchUsers(12));
+  }, []);
+
+  if (isCollectionsLoading || isUsersLoading) return <Preloader type={'preloader'} />;
+
   return (
     <>
       <Routes>
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -5,27 +5,11 @@ import CategoriesSection from '../../components/landing/categoriesSection';
 import Discover from '../../components/landing/discover';
 import Highlight from '../../components/landing/highlight';
 import Instructions from '../../components/landing/instructions';
-import Preloader from '../../components/utils/preloader';
 import SubscribeWidget from '../../components/landing/subscribeWidget';
-
-import { fetchCollections } from '../../store/reducers/collections';
-import { fetchUsers } from '../../store/reducers/users';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import LandingFooter from '../../components/landing/footer';
 import LandingHeader from '../../components/landing/header';
 
 const Main = () => {
-  const dispatch = useDispatch();
-  const isCollectionsLoading = useSelector((state) => state.collections.isLoading);
-  const isUsersLoading = useSelector((state) => state.users.isLoading);
-
-  useEffect(() => {
-    dispatch(fetchCollections(5));
-    dispatch(fetchUsers(12));
-  }, []);
-
-  if (isCollectionsLoading || isUsersLoading) return <Preloader type={'preloader'} />;
   return (
     <main>
       <div className='main'>
